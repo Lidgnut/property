b989cd71420b72cf9d5ee3c217dfc798544011bd
Marco Waack <marco-waack@web.de>
2019-04-15 00:22:17 +0200

    added some extra template parameters

--- Property/include/property/PropertyHolder.h
+++ Property/include/property/PropertyHolder.h
@@ -6,38 +6,38 @@
 
 namespace mwaack
 {
-
+	template<typename PropertyBase = I_Property, typename = std::enable_if_t<std::is_base_of_v<I_Property, PropertyBase>>>
 	class I_PropertyHolder
 	{
 	public:
 		virtual ~I_PropertyHolder() = default;
 
 
-		virtual I_Property& fastProp() = 0;
+		virtual PropertyBase& fastProp() = 0;
 
-		virtual I_Property& slowProp() = 0;
+		virtual PropertyBase& slowProp() = 0;
 
-		virtual const I_Property& fastProp() const = 0;
+		virtual const PropertyBase& fastProp() const = 0;
 
-		virtual const I_Property& slowProp() const = 0;
+		virtual const PropertyBase& slowProp() const = 0;
 
-		virtual const I_Property& cfastProp() const = 0;
+		virtual const PropertyBase& cfastProp() const = 0;
 
-		virtual const I_Property& cslowProp() const = 0;
+		virtual const PropertyBase& cslowProp() const = 0;
 	};
 
-	template <size_t Buffer>
-	class PropertyHolder : public I_PropertyHolder
+	template <size_t Buffer, typename PropertyBase = I_Property, typename = std::enable_if_t<std::is_base_of_v<I_Property, PropertyBase>>>
+	class PropertyHolder : public I_PropertyHolder<PropertyBase>
 	{
 		std::array<unsigned char, Buffer> m_buffer;
-		I_Property* m_fastProperty;
-		I_Property* m_slowProperty;
+		PropertyBase* m_fastProperty;
+		PropertyBase* m_slowProperty;
 
 	public:
 		template<typename T, typename U,
 			typename = std::enable_if_t<(sizeof(T) <= Buffer)>,
-			typename = std::enable_if_t<std::is_base_of_v<I_Property, T>>,
-			typename = std::enable_if_t<std::is_base_of_v<I_Property, U>>
+			typename = std::enable_if_t<std::is_base_of_v<PropertyBase, T>>,
+			typename = std::enable_if_t<std::is_base_of_v<PropertyBase, U>>
 		>
 			PropertyHolder(const T& t, const U& u)
 			: m_buffer()
@@ -48,8 +48,8 @@ namespace mwaack
 
 		template<typename T, typename U,
 			typename = std::enable_if_t<(sizeof(T) <= Buffer)>,
-			typename = std::enable_if_t<std::is_base_of_v<I_Property, T>>,
-			typename = std::enable_if_t<std::is_base_of_v<I_Property, U>>
+			typename = std::enable_if_t<std::is_base_of_v<PropertyBase, T>>,
+			typename = std::enable_if_t<std::is_base_of_v<PropertyBase, U>>
 		>
 			PropertyHolder(T&& t, U&& u)
 			: m_buffer()
@@ -63,7 +63,7 @@ namespace mwaack
 
 		PropertyHolder(PropertyHolder&& other)
 			: m_buffer(other.m_buffer)
-			, m_fastProperty((I_Property*)&m_buffer)
+			, m_fastProperty((PropertyBase*)&m_buffer)
 			, m_slowProperty(other.m_slowProperty)
 		{
 			other.m_slowProperty = nullptr;
@@ -72,7 +72,7 @@ namespace mwaack
 		PropertyHolder& operator=(PropertyHolder&& other)
 		{
 			m_buffer = other.m_buffer;
-			m_fastProperty = (I_Property*)&m_buffer;
+			m_fastProperty = (PropertyBase*)&m_buffer;
 
 			m_slowProperty = other.m_slowProperty;
 
@@ -90,32 +90,32 @@ namespace mwaack
 		}
 
 
-		I_Property& fastProp()
+		PropertyBase& fastProp()
 		{
 			return *m_fastProperty;
 		}
 
-		I_Property& slowProp()
+		PropertyBase& slowProp()
 		{
 			return *m_slowProperty;
 		}
 
-		const I_Property& fastProp() const
+		const PropertyBase& fastProp() const
 		{
 			return *m_fastProperty;
 		}
 
-		const I_Property& slowProp() const
+		const PropertyBase& slowProp() const
 		{
 			return *m_slowProperty;
 		}
 
-		const I_Property& cfastProp() const
+		const PropertyBase& cfastProp() const
 		{
 			return *m_fastProperty;
 		}
 
-		const I_Property& cslowProp() const
+		const PropertyBase& cslowProp() const
 		{
 			return *m_slowProperty;
 		}
